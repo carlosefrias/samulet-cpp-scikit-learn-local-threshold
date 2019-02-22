b930a5c1a13bb1d53f34974b299bfc9a114e63a7
Carlos Frias <carlosefrias@gmail.com>
2019-02-22 19:19:55 +0000

    conversion Mat to emxArray and back corrected

--- CallScikitLearnFormCPP/ConsoleApplication1/ConsoleApplication1.cpp
+++ CallScikitLearnFormCPP/ConsoleApplication1/ConsoleApplication1.cpp
@@ -15,7 +15,6 @@
 #include "Threshold_emxAPI.h"
 #include "Threshold_initialize.h"
 #include "adaptthresh.h"
-
 using namespace cv;
 using namespace std;
 //Adaptive Integral threshold
@@ -95,7 +94,6 @@ void thresholdIntegral(cv::Mat &inputMat, cv::Mat &outputMat)
 		}
 	}
 }
-
 PyObject *pName, *pModule, *pFunc, *pArgs, *pValue, *img, *blockSize, *method, *param, *offSet, *thresholdedImg, *ArgsArray;
 int pythonWrapper()
 {
@@ -250,9 +248,9 @@ double getMin(emxArray_real_T *matrix, int numRows, int numCols)
 	return min;
 }
 
-UINT16 getMax(emxArray_uint16_T *matrix, int numRows, int numCols)
+UINT16_T getMax(emxArray_uint16_T *matrix, int numRows, int numCols)
 {
-	UINT16 max = 0;
+	UINT16_T max = 0;
 	int k = 0;
 	for (size_t i = 0; i < numRows; i++)
 	{
@@ -268,9 +266,9 @@ UINT16 getMax(emxArray_uint16_T *matrix, int numRows, int numCols)
 	return max;
 }
 
-UINT16 getMin(emxArray_uint16_T *matrix, int numRows, int numCols)
+UINT16_T getMin(emxArray_uint16_T *matrix, int numRows, int numCols)
 {
-	UINT16 min = UINT16_MAX;
+	UINT16_T min = UINT16_MAX;
 	int k = 0;
 	for (size_t i = 0; i < numRows; i++)
 	{
@@ -286,6 +284,60 @@ UINT16 getMin(emxArray_uint16_T *matrix, int numRows, int numCols)
 	return min;
 }
 
+UINT16_T getMax(UINT16_T** a, int numRows, int numCols)
+{
+	UINT16_T max = 0;
+	for (size_t i = 0; i < numRows; i++)
+	{
+		for (size_t j = 0; j < numCols; j++)
+		{
+			if (a[i][j] > max)
+				max = a[i][j];
+		}
+	}
+	return max;
+}
+
+emxArray_uint16_T* Mat_TO_emxArray_uint16_T(Mat mat)
+{
+	int NumRows = mat.rows;
+	int NumCols = mat.cols;
+
+	UINT16_T **array = new UINT16_T*[mat.rows];
+	for (int i = 0; i < mat.rows; ++i)
+		array[i] = new UINT16_T[mat.cols];
+
+	for (int i = 0; i < mat.rows; ++i)
+		array[i] = mat.ptr<UINT16_T>(i);
+
+	//Converting the 2D long array (image) to a emxArray_uint16_T* (1st parameter of the Threshold function)
+	emxArray_uint16_T *X;
+	X = emxCreate_uint16_T(NumRows, NumCols);
+	for (size_t i = 0; i < NumRows; i++)
+	{
+		for (size_t j = 0; j < NumCols; j++)
+		{
+			X->data[NumRows * i + j] = array[i][j];
+		}
+	}
+	return X;
+}
+Mat emxArray_real_T_To_Mat(emxArray_real_T* Result, int NumRows, int NumCols) 
+{
+	//Convert Result to a Mat again and show the comparasion
+	double* resArray = new double[NumRows * NumCols];
+	for (size_t i = 0; i < NumRows * NumCols; i++)
+	{
+		resArray[i] = Result->data[i];
+	}
+
+	cout << "Matlab max position: " << resArray[517 * NumCols + 476] << endl;
+	cout << "Matlab min position: " << resArray[1023 * NumCols + 1023] << endl;
+
+	cv::Mat Res(NumRows, NumCols, CV_64F, resArray);
+	std::memcpy(Res.data, resArray, NumRows * NumCols * sizeof(double));
+	return Res;
+}
 //Using the matlab to c++ generated code for the adaptive threshold
 void usingMatlabGenCode()
 {
@@ -299,54 +351,8 @@ void usingMatlabGenCode()
 
 	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 	//Converting Mat object to a regular 2d array (probably there is a simpler way to do this)
-	int NumRows = mat.rows;
-	int NumCols = mat.cols;
-	
-	/*std::vector<UINT16> array;
-	if (mat.isContinuous())
-	{
-		array.assign(mat.datastart, mat.dataend);
-	}
-	else
-	{
-		for (int i = 0; i < mat.rows; ++i)
-		{
-			array.insert(array.end(), mat.ptr<UINT16>(i), mat.ptr<UINT16>(i) + mat.cols);
-		}
-	}
+	emxArray_uint16_T *X = Mat_TO_emxArray_uint16_T(mat);
 
-	UINT16** a = new UINT16*[NumRows];
-	for (int i = 0; i < NumRows; ++i)
-		a[i] = new UINT16[NumCols];
-
-	int k = 0;
-	for (size_t i = 0; i < NumRows; i++)
-	{
-		for (size_t j = 0; j < NumCols; j++)
-		{
-			a[i][j] = array[k];
-			k++;
-		}
-	}*/
-
-	//Converting the 2D long array (image) to a emxArray_uint16_T* (1st parameter of the Threshold function)
-	emxArray_uint16_T *X;
-	X = emxCreate_uint16_T(NumRows, NumCols);
-	for (size_t i = 0; i < NumRows; i++)
-	{
-		for (size_t j = 0; j < NumCols; j++)
-		{
-			//X->data[NumRows * i + j] = a[i][j];
-			//UINT16* val = new UINT16[1];
-			//val = mat.ptr<UINT16>(NumRows * i + j);
-			X->data[NumRows * i + j] = mat.ptr<UINT16>(NumRows * i + j);
-		}
-	}
-
-	/*for (size_t i = 0; i < X->size; i++)
-	{
-
-	}*/
 	//Result will contain the result of the matlab Threshold function
 	emxArray_real_T *Result;
 	emxInitArray_real_T(&Result, 2);
@@ -358,40 +364,19 @@ void usingMatlabGenCode()
 	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << std::endl;
 
+	Mat Res = emxArray_real_T_To_Mat(Result, mat.rows, mat.cols);
 
-	//TODO: convert Result to a Mat again and show the comparasion
-	cv::Mat outputMat = cv::Mat::zeros(mat.size(), CV_16U);
-	long *p_outputMat;
-	int k = 0;
-	for (size_t i = 0; i < NumRows; i++)
-	{
-		p_outputMat = outputMat.ptr<long>(i);
-		for (size_t j = 0; j < NumCols; j++)
-		{
-			//cout << Result->data[k] << endl;
-			p_outputMat[j] = Result->data[k];
-			k++;
-		}
-	}
 	double min, max;
 	cv::minMaxLoc(mat, &min, &max);
 	cout << "Original Mat:" << endl;
 	cout << "min: " << min << "\nmax: " << max << endl;
 	cout << "After converting to a emxArray_uint16_T :" << endl;
-	cout << "min: " << getMin(X, NumRows, NumCols) << "\nmax: " << getMax(X, NumRows, NumCols) << endl;
+	cout << "min: " << getMin(X, mat.rows, mat.cols) << "\nmax: " << getMax(X, mat.rows, mat.cols) << endl;
 	cout << "After Thresholding in a emxArray_real_T format:" << endl;
-	cout << "min: " << getMin(Result, NumRows, NumCols) << "\nmax: " << getMax(Result, NumRows, NumCols) << endl;
+	cout << "min: " << getMin(Result, mat.rows, mat.cols) << "\nmax: " << getMax(Result, mat.rows, mat.cols) << endl;
 	cout << "Final Mat:" << endl;
-	cv::minMaxLoc(outputMat, &min, &max);
+	cv::minMaxLoc(Res, &min, &max);
 	cout << "min: " << min << "\nmax: " << max << endl;
-
-	printf("end");
-	//// Show binary image
-	//cv::imshow("output image", outputMat);
-	////! [bin_2] 
-
-	//cv::waitKey(0);
-
 }
 
 int main()
@@ -403,18 +388,6 @@ int main()
 	//Using the matlab to c++ generated code for the adaptive threshold
 	usingMatlabGenCode();
 
-	//const char *filename = "2mmBrinell250_31.400_-1.000.tif";
-	//const char *filename = "t19452-09_27.100_-87.700.tif";
-	////Reading the image 
-	//cv::Mat mat = cv::imread(filename, IMREAD_UNCHANGED);
-	//cv::Mat bw2 = cv::Mat::zeros(mat.size(), CV_8UC1);
-	//thresholdIntegral(mat, bw2);
-
-	//// Show binary image
-	//cv::imshow("binary integral", bw2);
-	////! [bin_2] 
-
-	//cv::waitKey(0);
 	return 0;
 }
 
